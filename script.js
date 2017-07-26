a4cc386cef49c14d1b8663780e6716019f8145ee
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-07-26 16:01:37 -0500

    styled page, started defining classes

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('dom')
+});
+class Shape {
+  constructor(width, height) {
+        this.width = width;
+    this.height = height;
+  }
+};
+
+class Square extends Shape{
+    constructor(sideLength){
+        this.sideLength=sideLength
+    }
+};
+
+class Circle extends Shape{
+    constructor(radius){
+        this.radius=radius
+    }
+};
+
+class Triangle extends Shape{
+    constructor(height){
+        this.height=height
+    }
+};
+
+class Rectangle{
+    constructor(width, height){
+        this.width=width;
+        this.height=height;
+    }
+};
